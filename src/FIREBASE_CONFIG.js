2a969e4bcc41af1c1ce550f4ea3311aef6b87b8a
isman07 <isman07@yahoo.com>
2022-03-07 09:22:01 +0700

    V 0.0.1

---
+++ src/FIREBASE_CONFIG.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBUSVZtl7ew1M8kAQMf0nBCQ3RQN_Ct7k8",
+    authDomain: "office-new-b4d63.firebaseapp.com",
+    projectId: "office-new-b4d63",
+    storageBucket: "office-new-b4d63.appspot.com",
+    messagingSenderId: "298778647479",
+    appId: "1:298778647479:web:de0c19d8f3757462cd6632"
+};
---
+++ src/layout/dashboardContent/Posts.js
@@ -0,0 +1,62 @@
+import * as React from "react";
+import { 
+    List,
+    Datagrid,
+    TextField,
+    ReferenceField,
+    EditButton,
+    Edit,
+    Create,
+    SimpleForm,
+    ReferenceInput,
+    SelectInput,
+    TextInput,
+} from 'react-admin';
+
+const PostTitle = ({ record }) => {
+    return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
+const postFilters = [
+    <TextInput source="q" label="Search" alwaysOn />,
+    <ReferenceInput source="userId" label="User" reference="users" allowEmpty>
+        <SelectInput optionText="name" />
+    </ReferenceInput>,
+];
+
+export const PostList = props => (
+    <List filters={postFilters} {...props}>
+        <Datagrid rowClick="edit">
+            <ReferenceField source="userId" reference="users">
+                <TextField source="name" />
+            </ReferenceField>
+            <TextField source="title" />
+            <TextField source="body" />
+            <EditButton />
+        </Datagrid>
+    </List>
+);
+
+export const PostEdit = props => (
+    <Edit title={<PostTitle/>} {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Create>
+);
